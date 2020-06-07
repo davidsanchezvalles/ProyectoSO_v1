a0b05bfa6f223a7d178262bf2301c063ea15e8da
titusergio <sergiograslopez@gmail.com>
2020-06-07 21:19:47 +0200

    v3

--- servidor.c
+++ servidor.c
@@ -22,9 +22,18 @@ typedef struct {
 	int num;
 }ListaConectados;
 
-pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER; //Acceso excluyente
 
+typedef struct{
+	int identificador;
+	char jugador1[20];
+	char jugador2[20];
+}Cpartida;
+
+
+
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER; //Acceso excluyente
 ListaConectados lista;
+Cpartida partida;
 
 int Poner(ListaConectados *lista, char nombre[20], int socket){
 	//Anade nuevo conectado.
@@ -373,26 +382,31 @@ void *AtenderCliente(void *socket){
 				
 				int pos=DamePosicion(&lista,nombre_invitar);
 				printf("Invitando a %s con socket: %d\n",lista.conectados[pos].nombre, lista.conectados[pos].socket);
-				sprintf(buff2,"7/Desea jugar?");
-				write (lista.conectados[pos].socket,buff2, strlen(buff2)); 
+				sprintf(buff2,"7/El jugador %s le ha invitado a jugar,acepta el reto?", nombre);
+				write (lista.conectados[pos].socket,buff2, strlen(buff2));
+				strcpy(partida.jugador1, nombre); //el jugador que invita será el que tendrá el turno 1, el que empezará a jugar primero
 				
 				break;
 			}
 			
-			case 8: {	//respuesta
+			case 8: {	//respuestainvitacion
 				
 				char respuesta[4];
 				p = strtok( NULL, "/");
 				strcpy (respuesta, p);
 				if (strcmp(respuesta,"SI")==0)
 				{
-					sprintf(buff2,"8/Se acepto invitacion,comienza el juego");
-					for (i=0; i<lista.num; i++){  //enviamos este mensaje a todos los clientes
-						write (lista.conectados[i].socket,buff2, strlen(buff2)); 
-					}
-				}else
+					strcpy(partida.jugador2, nombre);  // el jugador que ha aceptado la invitacion será el jugador 2
+					sprintf(buff2,"8/%s/%s/1",partida.jugador1, partida.jugador2); //enviamos al jugador que invito, el jugador 1, los nombres de los jugadores de la partida y le indicamos que será su turno
+					int pos1 = DamePosicion(&lista, partida.jugador1);
+					write (lista.conectados[pos1].socket,buff2, strlen(buff2));
+					sprintf(buff2,"8/%s/%s/0",partida.jugador1, partida.jugador2); //enviamos al jugador 2 que no es su turno 
+					int pos2 = DamePosicion(&lista, partida.jugador2);
+					write (lista.conectados[pos2].socket,buff2, strlen(buff2));
+				
+				}else if (strcmp(respuesta,"NO")==0)
 				{
-					int pos=DamePosicion(&lista,nombre)	
+					int pos=DamePosicion(&lista,nombre);	
 					sprintf(buff2,"8/No se acepto invitacion");
 					//Enviamos la respuesta a mi cliente
 					write (lista.conectados[pos].socket,buff2, strlen(buff2)); 	
@@ -411,6 +425,37 @@ void *AtenderCliente(void *socket){
 				for (i=0; i<lista.num; i++){  //enviamos este mensaje a todos los clientes
 					write (lista.conectados[i].socket,buff2, strlen(buff2)); 
 				}
+				
+				break;
+				
+				
+			case 10:  //tiro
+				
+				p = strtok( NULL, "/"); 
+				int turno ;
+				turno = atoi(p);
+				p = strtok( NULL, "/"); 
+				char  Vx[20];
+				strcpy(Vx,p); 
+				p = strtok( NULL, "/"); 
+				char  Voy[20];
+				strcpy(Voy,p);
+				
+				
+				sprintf(buff2,"10/%d/%s/%s", turno,Vx, Voy); // le pasamos a los 2 jugadores los datos para simular el tiro del contrincante y su turno
+				printf("%s\n", buff2);
+				int p1 = DamePosicion(&lista, partida.jugador1);
+				int p2 = DamePosicion(&lista, partida.jugador2);
+				
+				if( turno==1)
+				{
+					write (lista.conectados[p2].socket,buff2, strlen(buff2));
+					
+				}else if ( turno==2)
+				{
+					write (lista.conectados[p1].socket,buff2, strlen(buff2));
+				}
+				
 				
 				break;
 			}
@@ -439,9 +484,9 @@ int main(int argc, char *argv[]) {
 	memset(&serv_adr, 0, sizeof(serv_adr));// inicialitza a zero serv_addr
 	serv_adr.sin_family = AF_INET;	// asocia el socket a cualquiera de las IP de la m?quina.
 	serv_adr.sin_addr.s_addr = htonl(INADDR_ANY); //htonl formatea el numero que recibe al formato necesario
-	serv_adr.sin_port = htons(9050); // escucharemos en el port 90X0
+	serv_adr.sin_port = htons(9040); // escucharemos en el port 90X0
 	if (bind(sock_listen, (struct sockaddr *) &serv_adr, sizeof(serv_adr)) < 0){
-		printf ("Error al bind");
+		printf ("Error al bind\n");
 	}
 	//La cola de peticiones pendientes no podr? ser superior a 4
 	if (listen(sock_listen, 2) < 0){
